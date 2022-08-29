794457d77add82f9770efa224cfa3f6251ff3a16
violeta atkinson <violeta.atkinson@gmail.com>
2022-08-29 20:01:27 +0200

    feat: authenticated

--- config/index.routes.js
+++ config/index.routes.js
@@ -4,6 +4,7 @@ const authController = require('../controllers/auth.controller')
 const miscController = require('../controllers/misc.controller')
 const usersController = require("../controllers/user.controller")
 const fileUploader = require('./cloudinary.config')
+const authMiddlewares = require("../middlewares/authMiddleware");
 
 const SCOPES = [
     "profile",
@@ -17,12 +18,14 @@ router.get("/", miscController.home)
 router.get("/register", authController.register);
 router.post("/register",fileUploader.single('image'), authController.doRegister);
 
-router.get("/login", authController.login);
+router.get("/login", authMiddlewares.isNotAuthenticated, authController.login);
 router.post("/login", authController.doLogin);
-router.get("/logout", authController.logout);
 
-router.get('/login/google', passport.authenticate('google-auth', { scope: SCOPES  }))
-router.get('/auth/google/callback', authController.doLoginGoogle)
+router.get('/login/google', authMiddlewares.isNotAuthenticated, passport.authenticate('google-auth', { scope: SCOPES  }))
+router.get('/auth/google/callback', authMiddlewares.isNotAuthenticated, authController.doLoginGoogle)
+
+router.get("/logout",authMiddlewares.isAuthenticated, authController.logout);
+ 
 router.get('/activate/:token', authController.activateAccount)
 // USERS
 router.get("/profile", usersController.profile);
