d4ee6cc13b5e836bfd9091cc7d6ef3b3f34df770
violeta atkinson <violeta.atkinson@gmail.com>
2022-08-30 18:12:36 +0200

    fix google auth

--- controllers/fest.controller.js
+++ controllers/fest.controller.js
@@ -1,10 +1,12 @@
 const Festival = require("../models/Festival.model");
 const mongoose = require("mongoose");
 
-module.exports.list =(req,res,next) => {
+module.exports.list = (req,res,next) => {
+    console.log("entro en list");
  Festival.find()
  .then((festivals) => {
-     res.render("festival/list",{festivals})
+     res.render("festival/list", {festivals})
  })
+ .catch(err => next(err))
 }
 
