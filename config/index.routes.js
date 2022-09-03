e27a8f71c56f9eb7cdb00eaf24f7aea791e2a5ea
Carmen Garcia <c.garcia.guzman2@gmail.com>
2022-09-03 10:45:01 +0200

    feat: fit viewa

--- config/index.routes.js
+++ config/index.routes.js
@@ -41,7 +41,8 @@ router.get("/festivals/create",authMiddlewares.isAuthenticated, festController.c
 router.post("/festivals/create",authMiddlewares.isAuthenticated,fileUploader.single('image'), festController.doCreate);
 
 router.get("/festivals", festController.list)
-router.post("/festivals/:id/comment", festController.comment)
+router.post("/festivals/:id/comment", authMiddlewares.isAuthenticated, festController.comment)
+//router.delete("/festivals/:id/comment", authMiddlewares.isAuthenticated, festController.deleteComment)
 router.get("/festivals/:id", festController.detail)
 
 
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,15 +4,11 @@ const Like = require("../models/Like.model")
 
 
 module.exports.profile = (req, res, next) => {
-  console.log("entro en profile controller");
-
   const userId = req.user._id.valueOf();
-  
+
   Like.find({ user: userId })
   .populate("festival")
   .then((likes) => {
-    console.log("entro en elthen")
-    console.log(likes)
     res.render("users/profile", { likes });
   })
   .catch( err => next( err ))
