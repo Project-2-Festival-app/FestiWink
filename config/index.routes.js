d2ac2e5dc9e79ecd28a84f15959e9670d6e76078
violeta atkinson <violeta.atkinson@gmail.com>
2022-09-05 18:28:21 +0200

    comment css

--- config/index.routes.js
+++ config/index.routes.js
@@ -41,8 +41,10 @@ router.get("/festivals/create",authMiddlewares.isAuthenticated, festController.c
 router.post("/festivals/create",authMiddlewares.isAuthenticated,fileUploader.single('image'), festController.doCreate);
 
 router.get("/festivals", festController.list)
+
 router.post("/comment/:id", authMiddlewares.isAuthenticated, festController.comment)
 router.post("/comment/:id/delete", authMiddlewares.isAuthenticated, festController.deleteComment)
+
 router.get("/festivals/:id", festController.detail)
 
 
