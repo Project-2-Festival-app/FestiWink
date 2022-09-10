ff874333b7b324cd312a957e5146b75fc97ca5ab
Carmen Garcia <c.garcia.guzman2@gmail.com>
2022-09-10 14:12:59 +0200

    fix

--- config/session.config.js
+++ config/session.config.js
@@ -4,14 +4,16 @@ const { DB } = require("./db.config");
 
 const sessionMaxAge = process.env.SESSION_AGE || 7; 
 
+console.log(process.env.COOKIE_SECURE)
+
 const sessionConfig = expressSession({
   secret: process.env.COOKIE_SECRET || "Super secret (change it!)",
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.COOKIE_SECURE ? true : false,
+    secure: process.env.COOKIE_SECURE === "true" || false,
     maxAge: 24 * 60 * 60 * 1000 * sessionMaxAge, 
-    httpOnly: true,
+    httpOnly: true
   },
   store: new MongoStore({
     mongoUrl: DB,
